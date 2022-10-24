19e869492293c0ecb0dc02b1cc0d79b9e69f2b35
MrBElga <43758059+MrBElga@users.noreply.github.com>
2022-10-23 23:09:00 -0300

    att
    atualizando repositorio

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app     = express();
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 app.get("/",(req,res)=>{
-    res.send("Bem vindo ao meu site");
+    res.render("index");
+});
+
+app.get("/perguntas", (req,res)=>{
+    res.render("pergunta");
 });
 
 app.listen(8080,()=>{
-    console.log("Iniciou!");
+    console.log("Iniciou! O_O << LEO ESTALA COOL");
 });
